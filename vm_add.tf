31fba6a2b9eba708ac6401d9f68ec487300891d6
unknown <sanjay.sabnis@gmail.com>
2018-07-09 10:51:15 +0100

    Terraform file to create a VM

---
+++ vm_add.tf
@@ -0,0 +1,24 @@
+resource "virtualbox_vm" "node" {
+    count = 2
+    name = "${format("node-%02d", count.index+1)}"
+    url = "https://vagrantcloud.com/ubuntu/boxes/trusty64/versions/20180206.0.0/providers/virtualbox.box"
+    image = "./virtualbox-ubuntu.box"
+    cpus = 2
+    memory = "512 mib",
+    user_data = "${file("user_data")}"
+
+
+    network_adapter {
+        type = "bridged",
+        host_interface = "en0",
+
+    }
+
+
+}
+output "IPAddr" {
+    value = "${element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 1)}"
+}
+output "IPAddr_2" {
+    value = "${element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 2)}"
+}
